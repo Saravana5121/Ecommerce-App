69f5d764d01dacbcda05c1d0c093ec063f4c787a
Sana <darkking7806@gmail.com>
2023-12-21 21:43:02 +0530

    Redux setup and Navigation,Login frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import { Route, RouterProvider, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+
+//Auth
+import Login from "./pages/Auth/login.jsx";
+
+
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<App />} />)
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path='/login' element={<Login />} />
+    </Route>
+    )
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
