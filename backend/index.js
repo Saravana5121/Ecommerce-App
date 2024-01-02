f7245ce93615476684251a4e3899266104ac6751
Sana <darkking7806@gmail.com>
2024-01-02 20:49:58 +0530

    Category init Folders

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import express from "express";
 //utils
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/api/users',userRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/category", categoryRoutes);
 
 app.listen(port, () => console.log(`server running on port: ${port} 🏃‍♂️`));
