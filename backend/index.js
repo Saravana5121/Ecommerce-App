81a37773815d058c4a62c4a4630466d26b0ee931
Sana <darkking7806@gmail.com>
2024-01-15 12:08:27 +0530

    Product route controllers craeted successfully

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,9 @@ import express from "express";
 
 //utils
 import connectDB from "./config/db.js";
-import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -21,5 +22,6 @@ app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => console.log(`server running on port: ${port} 🏃‍♂️`));
