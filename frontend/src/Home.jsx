d219e90bae206e9f903424590cc97c562c3e086c
Sana <darkking7806@gmail.com>
2024-01-31 11:17:44 +0530

    home

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -3,12 +3,45 @@ import { useGetProductsQuery } from "./redux/api/productApiSlice";
 import Loader from "./components/Loader";
 import Header from "./components/Header";
 import Message from "./components/Message";
+import Product from "./pages/Products/Product";
 
 const Home = () => {
   const { keyword } = useParams();
   const { data, isLoading, isError } = useGetProductsQuery({ keyword });
 
-  return <>{!keyword ? <Header /> : null}</>;
+  return (
+    <>
+      {!keyword ? <Header /> : null}
+      {isLoading ? (
+        <Loader />
+      ) : isError ? (
+        <Message variant="danger">
+          {isError?.data.message || isError.error}
+        </Message>
+      ) : (
+        <>
+          <div className="flex justify-between items-center">
+            <h1 className="ml-[15rem] mt-[5rem] text-[3rem]">
+              Special Products
+            </h1>
+            <Link
+              to="/shop"
+              className="bg-black font-bold rounded-lg py-2 px-10 mr-[15rem] mt-[5rem] text-white hover:bg-tblue"
+            >
+              Shop
+            </Link>
+            <div className="flex flex-wrap justify-center mt-[2rem]">
+              {data.products.map((product) => (
+                <div key={product._id}>
+                  <Product product={product} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
 export default Home;
---
+++ frontend/src/pages/Products/Product.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+const Product = () => {
+  return (
+    <div className=""></div>
+  )
+}
+
+export default Product
