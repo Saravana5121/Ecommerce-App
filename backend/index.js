b81cc3cadd82a8fcb903e2a768beef6886782b94
Sana <darkking7806@gmail.com>
2024-03-01 22:20:21 +0530

    Order management Backend finished

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -25,10 +26,10 @@ app.use(cookieParser());
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
-app.use("/api/upload",uploadRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api/orders", orderRoutes);
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
-
 
 app.listen(port, () => console.log(`server running on port: ${port} 🏃‍♂️`));
